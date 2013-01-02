50097ae8900f994b63c786fe8531f6d4996effaa
Freddy Trancart <freddy.trancart@ysance.com>
2013-01-02 16:51:34 +0100

    Using test views.

--- site/notepanel/test.py
+++ site/notepanel/test.py
@@ -6,6 +6,28 @@ from . import app
 @app.route("/", methods=["GET"])
 def index():
     return flask.render_template("panel.html")
+    
+@app.route("/login", methods=["POST"])
+def login():
+    return flask.jsonify(identified=True)
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    flask.session.pop("username", None)
+    return flask.render_template("panel.html")
+
+@app.route("/identify", methods=["GET"])
+def identify():
+    # TODO: db select
+    if "username" in flask.session:
+        return flask.jsonify(
+            identified=True,
+            username=flask.session["username"],
+            email=None,
+            id=None,
+            boards=None)
+    else:
+        return flask.jsonify(identified=False)
 
 @app.route("/test", methods=["GET"])
 def test():
