147230e8eea24662a234f44b59e38353445c2401
Freddy Trancart <freddy.trancart@ysance.com>
2013-01-03 14:48:38 +0100

    Debugging logs configuration.

--- site/notepanel/test.py
+++ site/notepanel/test.py
@@ -29,6 +29,7 @@ def identify():
     else:
         return flask.jsonify(identified=False)
 
+'''
 @app.route("/test", methods=["GET"])
 def test():
     import os
@@ -37,3 +38,4 @@ def test():
     else:
         myvar = 'local'
     return flask.render_template('test.html', myvar=myvar)
+'''
