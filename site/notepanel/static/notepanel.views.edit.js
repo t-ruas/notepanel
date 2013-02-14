2bad08a010014df91fb32f672a7f6f53f59a5321
Timothee Ruas <timothee.ruas@ysance.com>
2013-02-14 16:26:21 +0100

    note delete + notemenu reorg

--- site/notepanel/static/notepanel.views.edit.js
+++ site/notepanel/static/notepanel.views.edit.js
@@ -55,6 +55,7 @@ notepanel.views.edit = function (me) {
     
     var onCloseEdit = function () {
         me.disable();
+        notepanel.views.panel.unlock();
     };
     
     var onKeyUp = function (e) {
